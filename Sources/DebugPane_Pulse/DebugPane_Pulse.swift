7457fdbc2a26d643d2ceb5645a9e79225e1072c0
An Tran <tran.binhan@gmail.com>
2022-03-22 20:46:08 +0800

    Initial commit

--- Sources/DebugPane_Pulse/DebugPane_Pulse.swift
+++ Sources/DebugPane_Pulse/DebugPane_Pulse.swift
@@ -1,6 +1,35 @@
-public struct DebugPane_Pulse {
-    public private(set) var text = "Hello, World!"
+import Combine
+import Foundation
+import Pulse
+import SwiftUI
+import TweakPane
+import UIKit
+import PulseUI
 
-    public init() {
+public struct PulseBlade: Blade {
+    public var name: String = "Pulse"
+    
+    private weak var presentingViewController: UIViewController?
+    
+    public init(_ presentingViewController: UIViewController?) {
+        self.presentingViewController = presentingViewController
+    }
+    
+    public func render() -> AnyView {
+        AnyView(ContentView(action: {presentingViewController?.present($0, animated: true)} ))
+    }
+}
+
+private struct ContentView: View {
+    var action: (UIViewController) -> Void
+    var body: some View {
+        Button(
+            action: {
+                action(UIHostingController(rootView: MainView()))
+            },
+            label: {
+                Text("Show Pulse")
+            }
+        )
     }
 }
