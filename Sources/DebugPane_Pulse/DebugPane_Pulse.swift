e93277211e08103ff29500a8d1517330ade81747
antranapp <tran.binhan@gmail.com>
2022-03-23 20:25:14 +0800

    Add Demo app

--- Sources/DebugPane_Pulse/DebugPane_Pulse.swift
+++ Sources/DebugPane_Pulse/DebugPane_Pulse.swift
@@ -13,27 +13,29 @@ import UIKit
 public struct PulseBlade: Blade {
     public var name: String? = "Pulse"
     
-    private weak var presentingViewController: UIViewController?
+    private var presentingViewController: () -> UIViewController?
     
-    public init(_ presentingViewController: UIViewController?) {
+    public init(presentingViewController: @escaping () -> UIViewController?) {
         self.presentingViewController = presentingViewController
     }
     
     public func render() -> AnyView {
-        AnyView(ContentView(action: { presentingViewController?.present($0, animated: true) }))
+        AnyView(ContentView(action: { presentingViewController()?.present($0, animated: true) }))
     }
 }
 
 private struct ContentView: View {
     var action: (UIViewController) -> Void
     var body: some View {
-        Button(
-            action: {
-                action(UIHostingController(rootView: MainView()))
-            },
-            label: {
-                Text("Show Pulse")
-            }
-        )
+        VStack {
+            Button(
+                action: {
+                    action(UIHostingController(rootView: MainView()))
+                },
+                label: {
+                    Text("Show Pulse")
+                }
+            )
+        }
     }
 }
